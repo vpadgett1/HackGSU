b5adb353eff12ab5d1e6a965a3f912f8f802ee3b
vpadgett1 <vpadgett1@student.gsu.edu>
2022-03-19 14:02:09 -0400

    updated the databases to match code and updated more onboarding

--- models.py
+++ models.py
@@ -4,18 +4,19 @@ from flask_login import UserMixin
 
 class student_users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(80),  nullable=False)
     name = db.Column(db.String(100), nullable=False)
     education_level = db.Column()  #drop down element - need to store
     student_email = db.Column(db.String(100), nullable=False)
     student_class = db.relationship(
         "teacher_users", backref="student_users", lazy=True, passive_deletes=True
     )
-    parent_id = db.Column(db.Integer, db.ForeignKey("parent_users.id", ondelete="CASCADE"))
+    parent_id = db.relationship(
+        "parent_users", backref="student_users", lazy=True, passive_deletes=True
+    )
     parent_name = db.Column(db.String(100))
     parent_email = db.Column(db.String(100))
-    pre_lang_parent =  
+    parent_phone = db.Column(db.String(10), nullable=False)
+    pre_lang_parent = db.Column(db.String(50),  nullable=False)
     friend = db.relationship(
         "create_friend", backref="student_users", lazy=True, passive_deletes=True
     )
@@ -25,11 +26,10 @@ class parent_users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parent_name = db.Column(db.String(100), nullable=False)
     parent_email = db.Column(db.String(100), nullable=False)
-    pre_lang_parent =  
-    education_level = 
-    child = db.relationship(
-        "student_users", backref="parent_users", lazy=True, passive_deletes=True
-    )
+    parent_phone = db.Column(db.String(10), nullable=False)
+    pre_lang_parent = db.Column(db.String(50),  nullable=False)
+    education_level = db.Column(db.String(50))
+    child = db.Column(db.Integer, db.ForeignKey("student_users.name", ondelete="CASCADE"))
 
 class teacher_users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,6 +37,7 @@ class teacher_users(db.Model):
     teacher_email = db.Column(db.String(100), nullable=False)
     pre_lang_parent = db.Column(db.String(100))
     classroom_grade_level = db.Column(db.String(100), nullable=False)
+    teacher_phone = db.Column(db.String(10), nullable=False)
     classroom_id = db.Column(db.String(50), db.ForeignKey("parent_users.id", ondelete="CASCADE"))
 
 class create_friend(db.Model):
--- src/Pages/onboarding.js
+++ src/Pages/onboarding.js
@@ -156,6 +156,131 @@ const OnboardingPage = () => {
 
   const createTeacherUserAccount = async () => {
     // get input
+    const teacher_name = document.getElementById('name').value;
+    const pre_lang_parent = document.getElementById('yelpRestaurantID').value;
+    const classroom_grade_level = document.getElementById('education').value;
+    const classroom_id = document.getElementById('classroom_id').value;
+    const teacher_phone = document.getElementById('teach_phone').value;
+
+    // send to database, wait until db responds before continueing
+    await fetch(`/createAccount?zipcode=${zipcode}&yelpID=${yelpID}&username=${username}`, {
+      method: 'POST',
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.status && result.status === 200) {
+          if (result.newAccountCreated) {
+            navigate('/module');
+          } else { // show alert
+            setAlertMessage(result.message);
+            setShowMessage(true);
+          }
+        }
+      })
+      .catch((response) => console.log(response));
+  };
+
+  function renderTeacherUserQuestions() {
+    return (
+      <>
+        <div>Your Name:</div>
+        <input type="text" placeholder="name" id="name" maxLength={20} />
+        <div>Grade Level of Your Classroom:</div>
+        <select data-placeholder="Choose a Grade Level:" id="education">
+          <option>Kindergarden</option>
+          <option>1st Grade</option>
+          <option>2nd Grade</option>
+          <option>3rd Grade</option>
+          <option>4th Grade</option>
+          <option>5th Grade</option>
+        </select>
+        <div>Classroom Code (This Will Be Given to You Buy Your Teacher to Join Their Class):</div>
+        <input type="text" placeholder="classroom_id" id="classroom_id" maxLength={100} />
+        <div>Your Phone Number: (Do not Include Dashes)</div>
+        <input type="text" placeholder="teach_phone" id="teach_phone" maxLength={10} />
+        <div>Your Preferred Language for Communications:</div>
+        <select data-placeholder="Choose a Language..." id="teach_pref_lang">
+          <option value="Afrikaans">Afrikaans</option>
+          <option value="Albanian">Albanian</option>
+          <option value="Arabic">Arabic</option>
+          <option value="Armenian">Armenian</option>
+          <option value="Basque">Basque</option>
+          <option value="Bengali">Bengali</option>
+          <option value="Bulgarian">Bulgarian</option>
+          <option value="Catalan">Catalan</option>
+          <option value="Cambodian">Cambodian</option>
+          <option value="Chinese (Mandarin)">Chinese (Mandarin)</option>
+          <option value="Croatian">Croatian</option>
+          <option value="Czech">Czech</option>
+          <option value="Danish">Danish</option>
+          <option value="Dutch">Dutch</option>
+          <option value="English">English</option>
+          <option value="Estonian">Estonian</option>
+          <option value="Fiji">Fiji</option>
+          <option value="Finnish">Finnish</option>
+          <option value="French">French</option>
+          <option value="Georgian">Georgian</option>
+          <option value="German">German</option>
+          <option value="Greek">Greek</option>
+          <option value="Gujarati">Gujarati</option>
+          <option value="Hebrew">Hebrew</option>
+          <option value="Hindi">Hindi</option>
+          <option value="Hungarian">Hungarian</option>
+          <option value="Icelandic">Icelandic</option>
+          <option value="Indonesian">Indonesian</option>
+          <option value="Irish">Irish</option>
+          <option value="Italian">Italian</option>
+          <option value="Japanese">Japanese</option>
+          <option value="Javanese">Javanese</option>
+          <option value="Korean">Korean</option>
+          <option value="Latin">Latin</option>
+          <option value="Latvian">Latvian</option>
+          <option value="Lithuanian">Lithuanian</option>
+          <option value="Macedonian">Macedonian</option>
+          <option value="Malay">Malay</option>
+          <option value="Malayalam">Malayalam</option>
+          <option value="Maltese">Maltese</option>
+          <option value="Maori">Maori</option>
+          <option value="Marathi">Marathi</option>
+          <option value="Mongolian">Mongolian</option>
+          <option value="Nepali">Nepali</option>
+          <option value="Norwegian">Norwegian</option>
+          <option value="Persian">Persian</option>
+          <option value="Polish">Polish</option>
+          <option value="Portuguese">Portuguese</option>
+          <option value="Punjabi">Punjabi</option>
+          <option value="Quechua">Quechua</option>
+          <option value="Romanian">Romanian</option>
+          <option value="Russian">Russian</option>
+          <option value="Samoan">Samoan</option>
+          <option value="Serbian">Serbian</option>
+          <option value="Slovak">Slovak</option>
+          <option value="Slovenian">Slovenian</option>
+          <option value="Spanish">Spanish</option>
+          <option value="Swahili">Swahili</option>
+          <option value="Swedish ">Swedish </option>
+          <option value="Tamil">Tamil</option>
+          <option value="Tatar">Tatar</option>
+          <option value="Telugu">Telugu</option>
+          <option value="Thai">Thai</option>
+          <option value="Tibetan">Tibetan</option>
+          <option value="Tonga">Tonga</option>
+          <option value="Turkish">Turkish</option>
+          <option value="Ukrainian">Ukrainian</option>
+          <option value="Urdu">Urdu</option>
+          <option value="Uzbek">Uzbek</option>
+          <option value="Vietnamese">Vietnamese</option>
+          <option value="Welsh">Welsh</option>
+          <option value="Xhosa">Xhosa</option>
+      </select>
+        <br />
+        <button type="button" onClick={createTeacherUserAccount} className="continueButton">Continue</button>
+      </>
+    );
+  }
+
+  const createParentUserAccount = async () => {
+    // get input
     const yelpID = document.getElementById('yelpRestaurantID').value;
     const zipcode = document.getElementById('zipCodeInput').value;
     const username = document.getElementById('merchantUserName').value;
@@ -178,17 +303,99 @@ const OnboardingPage = () => {
       .catch((response) => console.log(response));
   };
 
-  function renderTeacherUserQuestions() {
+  function renderParentUserQuestions() {
     return (
       <>
-        <div>Please enter your zip code</div>
-        <input type="text" placeholder="Zip Code" id="zipCodeInput" maxLength={20} />
-        <div>Please enter your yelp restaurant id</div>
-        <input type="text" placeholder="Yelp Restaurant ID" id="yelpRestaurantID" maxLength={100} />
-        <div>Please enter a username</div>
-        <input type="text" placeholder="Username" id="merchantUserName" maxLength={100} />
+        <div>Your Name:</div>
+        <input type="text" placeholder="name" id="name" maxLength={20} />
+        <div>Choose Your Understanding Level of AI:</div>
+        <select data-placeholder="Choose a Experience Level:" id="education">
+          <option>Little to None</option>
+          <option>Beginner</option>
+          <option>Moderate</option>
+          <option>Advanced</option>
+        </select>
+        <div>Classroom Code (This Will Be Given to You Buy Your Teacher to Join Their Class):</div>
+        <input type="text" placeholder="classroom_id" id="classroom_id" maxLength={100} />
+        <div>Your Phone Number: (Do not Include Dashes)</div>
+        <input type="text" placeholder="teach_phone" id="teach_phone" maxLength={10} />
+        <div>Your Preferred Language for Communications:</div>
+        <select data-placeholder="Choose a Language..." id="teach_pref_lang">
+          <option value="Afrikaans">Afrikaans</option>
+          <option value="Albanian">Albanian</option>
+          <option value="Arabic">Arabic</option>
+          <option value="Armenian">Armenian</option>
+          <option value="Basque">Basque</option>
+          <option value="Bengali">Bengali</option>
+          <option value="Bulgarian">Bulgarian</option>
+          <option value="Catalan">Catalan</option>
+          <option value="Cambodian">Cambodian</option>
+          <option value="Chinese (Mandarin)">Chinese (Mandarin)</option>
+          <option value="Croatian">Croatian</option>
+          <option value="Czech">Czech</option>
+          <option value="Danish">Danish</option>
+          <option value="Dutch">Dutch</option>
+          <option value="English">English</option>
+          <option value="Estonian">Estonian</option>
+          <option value="Fiji">Fiji</option>
+          <option value="Finnish">Finnish</option>
+          <option value="French">French</option>
+          <option value="Georgian">Georgian</option>
+          <option value="German">German</option>
+          <option value="Greek">Greek</option>
+          <option value="Gujarati">Gujarati</option>
+          <option value="Hebrew">Hebrew</option>
+          <option value="Hindi">Hindi</option>
+          <option value="Hungarian">Hungarian</option>
+          <option value="Icelandic">Icelandic</option>
+          <option value="Indonesian">Indonesian</option>
+          <option value="Irish">Irish</option>
+          <option value="Italian">Italian</option>
+          <option value="Japanese">Japanese</option>
+          <option value="Javanese">Javanese</option>
+          <option value="Korean">Korean</option>
+          <option value="Latin">Latin</option>
+          <option value="Latvian">Latvian</option>
+          <option value="Lithuanian">Lithuanian</option>
+          <option value="Macedonian">Macedonian</option>
+          <option value="Malay">Malay</option>
+          <option value="Malayalam">Malayalam</option>
+          <option value="Maltese">Maltese</option>
+          <option value="Maori">Maori</option>
+          <option value="Marathi">Marathi</option>
+          <option value="Mongolian">Mongolian</option>
+          <option value="Nepali">Nepali</option>
+          <option value="Norwegian">Norwegian</option>
+          <option value="Persian">Persian</option>
+          <option value="Polish">Polish</option>
+          <option value="Portuguese">Portuguese</option>
+          <option value="Punjabi">Punjabi</option>
+          <option value="Quechua">Quechua</option>
+          <option value="Romanian">Romanian</option>
+          <option value="Russian">Russian</option>
+          <option value="Samoan">Samoan</option>
+          <option value="Serbian">Serbian</option>
+          <option value="Slovak">Slovak</option>
+          <option value="Slovenian">Slovenian</option>
+          <option value="Spanish">Spanish</option>
+          <option value="Swahili">Swahili</option>
+          <option value="Swedish ">Swedish </option>
+          <option value="Tamil">Tamil</option>
+          <option value="Tatar">Tatar</option>
+          <option value="Telugu">Telugu</option>
+          <option value="Thai">Thai</option>
+          <option value="Tibetan">Tibetan</option>
+          <option value="Tonga">Tonga</option>
+          <option value="Turkish">Turkish</option>
+          <option value="Ukrainian">Ukrainian</option>
+          <option value="Urdu">Urdu</option>
+          <option value="Uzbek">Uzbek</option>
+          <option value="Vietnamese">Vietnamese</option>
+          <option value="Welsh">Welsh</option>
+          <option value="Xhosa">Xhosa</option>
+        </select>
         <br />
-        <button type="button" onClick={createTeacherUserAccount} className="continueButton">Continue</button>
+        <button type="button" onClick={createParentUserAccount} className="continueButton">Continue</button>
       </>
     );
   }
