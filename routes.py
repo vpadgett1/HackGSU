8ef24ccbc613059b3802b46c47bd167ff3aea2b4
jarrywc <66788651+jarrywc@users.noreply.github.com>
2022-03-20 09:37:52 -0400

    Added signup, fixed flask port

--- routes.py
+++ routes.py
@@ -438,5 +438,5 @@ def main():
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 5000)), debug=True
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
     )
