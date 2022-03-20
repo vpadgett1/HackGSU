1ce90009080373dfc21757f28cba15726fd7c9e1
abbaasalif <abbaasalif@yahoo.com>
2022-03-20 09:05:23 -0400

    module 1 content

---
+++ src/Pages/module1pages/page3.js
@@ -0,0 +1,22 @@
+import '../module1pages/module1styling/style.css';
+function Module1Page3 () {
+
+  return (
+    <div>
+      <div>
+        <h2> Types of AI </h2>
+      </div>
+      <div> There are two major versions of AI based on their domain</div>
+      <div>
+      <ul>       
+        <li><strong> Artificial General Intelligence</strong> - <p class='lead'>It means AI is designed to work for any kind of problem.
+</p></li>
+        <li><strong> Artificial Narrow Intelligence</strong> - <p class='lead'>It means the AI is designed to work for a specific problem.
+</p></li>
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Module1Page3;
